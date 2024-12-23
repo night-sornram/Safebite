05e738ad3c949df855add09e04e74030dc1fd243
night-sornram <6633236021@student.chula.ac.th>
2024-12-23 21:22:41 +0700

    feat: load environment variables and remove Vercel configuration

--- server/main.go
+++ server/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"log"
+
+	"github.com/joho/godotenv"
 	"github.com/night-sornram/Safebite/app"
 )
 
 func main() {
+	errEnv := godotenv.Load()
+	if errEnv != nil {
+		log.Fatal("Error loading .env file")
+	}
 	// setup and run app
 	err := app.SetupAndRunApp()
 	if err != nil {
